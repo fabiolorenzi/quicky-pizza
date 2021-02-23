a2c88374f1dc55ddcaff297c7ad2d94b0613b92e
fabiolorenzi <fabiolorenzi@gmail.com>
2021-02-23 09:27:25 +0000

    implementing burger button and devices compatibility

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Menu() {
+    const [open, setOpen] = useState(false);
+
+    const openClose = e => {
+        e.preventDefault();
+        setOpen(!open);
+    };
+
+    let openCloseRes = open ? "burgerOpen" : "";
+
     return(
         <div className="menu">
-            <a href="#aboutus"><p>About us</p></a>
-            <a href="#shop"><p>Shop</p></a>
-            <a href="#checkout"><p>Checkout</p></a>
-            <a href="#contacts"><p>Contacts</p></a>
+            <div className="big_menu">
+                <a href="#aboutus"><p>About us</p></a>
+                <a href="#shop"><p>Shop</p></a>
+                <a href="#checkout"><p>Checkout</p></a>
+                <a href="#contacts"><p>Contacts</p></a>
+            </div>
+            <div className="burger_menu">
+                <button className={openCloseRes} onClick={openClose}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </button>
+            </div>
         </div>
     );
 };
